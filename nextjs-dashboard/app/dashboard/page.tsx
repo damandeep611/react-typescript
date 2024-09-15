8313c21681c4462293e85cfe303820f594f2804f
devDaman <damandeep611@gmail.com>
2024-09-15 16:00:19 +0530

    nextjs dashboard

---
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Customers from "./customers/page";
+import Invoices from "./invoices/page";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>This is the Dashboard page</h1>
+    </div>
+  );
+}
