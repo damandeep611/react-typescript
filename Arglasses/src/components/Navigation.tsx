a3c3c2d7c7f16efe11e66ed833dc4cbdb6b18818
devDaman <damandeep611@gmail.com>
2025-01-01 15:35:45 +0530

    ai glasses landing page started

---
+++ Arglasses/src/components/Navigation.tsx
@@ -0,0 +1,33 @@
+import { Heart, Search, ShoppingBag, User } from "lucide-react";
+import { navigationItems } from "../data/navigation";
+
+export default function Navigation() {
+  return (
+    <header className="flex  items-center justify-between h-16 px-8">
+      <div>
+        <a href="#" className="uppercase text-xl tracking-widest font-semibold">
+          Glasses.com
+        </a>
+      </div>
+      <nav className="flex items-center justify-between  text-lg gap-8 ">
+        {navigationItems.map((item) => (
+          <a href={item.href}>{item.title}</a>
+        ))}
+      </nav>
+      <div className="flex items-center justify-between gap-3">
+        <span>
+          <Search />
+        </span>
+        <span>
+          <Heart />
+        </span>
+        <span>
+          <User />
+        </span>
+        <span>
+          <ShoppingBag />
+        </span>
+      </div>
+    </header>
+  );
+}
---
+++ Arglasses/src/data/navigation.ts
@@ -0,0 +1,8 @@
+export const navigationItems = [
+  { title: "GH", href: "/GH" },
+  { title: "Brands", href: "/brands" },
+  { title: "Offers", href: "/offers" },
+  { title: "Services", href: "services" },
+  { title: "Lenses", href: "/lenses" },
+  { title: "Sync Insurance", href: "/sync" },
+];
